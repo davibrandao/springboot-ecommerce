33b51346a08ac198efdba8e61780940e9d19325c
davibrandao <davi.lauritzen1@gmail.com>
2023-06-28 14:41:20 -0300

    Test

--- src/main/java/com/daviqueiroz/springbootecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/daviqueiroz/springbootecommerce/config/MyDataRestConfig.java
@@ -6,6 +6,7 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+// *
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
